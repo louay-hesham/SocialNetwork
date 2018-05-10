5ac9169f62d61c4086d4a2cc38e22f29f2559750
Louay Morsi <louay.hesham@gmail.com>
2018-05-10 23:19:24 +0200

    Frontend: Added extra elements for registeration page

--- frontend/src/app/classes/user.ts
+++ frontend/src/app/classes/user.ts
@@ -2,16 +2,37 @@ import * as crypto from 'crypto-js';
 
 export class User {
 
+  public firstName: string;
+  public lastName: string;
+  public nickname: string;
   public password: string;
+  public phone: string;
   public email: string;
+  public gender: boolean;
+  public birthdate: Date;
+  public profilePic: any;
+  public hometown: string;
+  public maritalStatus: number;
+  public aboutMe: string;
+
 
   constructor () { }
   
   public toJSON(): any {
     let hashedPwd = crypto.SHA256(this.password)
     return {
+      'firstName': this.firstName,
+      'lastName': this.lastName,
+      'nickname': this.nickname,
       'password': hashedPwd,
-      'email': this.email
+      'phone': this.phone,
+      'email': this.email,
+      'gender': this.gender,
+      'birthdate': this.birthdate,
+      'profilePic': this.profilePic,
+      'hometown': this.hometown,
+      'maritalStatus': this.maritalStatus,
+      'aboutMe': this.aboutMe
     }
   }
 
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../classes/user'
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,10 @@ export class LoginComponent implements OnInit {
   private loginEmail: string;
   private loginPassword: string;
 
+  private user: User = new User();
+  private emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  private repeatedPassword: string;
+
   constructor() { }
 
   ngOnInit() { }
@@ -18,6 +23,36 @@ export class LoginComponent implements OnInit {
     console.log(this.loginEmail);
     console.log(this.loginPassword);
     // to be modified
+  }
+
+  private register() {
+    console.log(this.user.toJSON())
+  }
+
+  private validateEmail(): boolean {
+    return this.emailRegex.test(this.user.email) || this.user.email == undefined;
+  }
+
+  private emailClass(): string {
+    let classes = 'form-control';
+    if (this.validateEmail()) {
+      return classes + ' ng-valid';
+    } else {
+      return classes + ' ng-invalid';
+    }
+  }
+
+  private checkPassword(): boolean {
+    return this.user.password == this.repeatedPassword;
+  }
+
+  private repPwdClass(): string {
+    let classes = 'form-control';
+    if (this.checkPassword()) {
+      return classes + ' ng-valid';
+    } else {
+      return classes + ' ng-invalid';
+    }
   }
 
 }
